1b7590f590e06be6869867a70015e59294405ce5
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2018-01-05 10:19:02 +0100

    Added libgdx sub project for the prototype

---
+++ libgdx/core/src/com/github/yin/spaceassets/game/SpaceAssetsGame.java
@@ -0,0 +1,37 @@
+package com.github.yin.spaceassets.game;
+
+import java.io.File;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SpaceAssetsGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		System.out.println(new File("badlogic.jpg").getAbsolutePath()); 
+		img = new Texture(new FileHandle(new File("badlogic.jpg")));
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ libgdx/desktop/src/com/github/yin/spaceassets/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.yin.spaceassets.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.yin.spaceassets.game.SpaceAssetsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceAssetsGame(), config);
+	}
+}
